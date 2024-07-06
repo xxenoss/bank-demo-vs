48f781ff12ee8f6bac019e7ed913edd67e724059
XXenoss <126971204+xxenoss@users.noreply.github.com>
2024-07-06 16:09:22 +0600

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const password = "1234"
 
 document.querySelector(".login-button").addEventListener("click", function(){
     if (document.querySelector(".username-input").value === username && document.querySelector(".password-input").value===password){
-        window.location.href = "/bank.html"
+        window.location.href = "bank.html"
     }
     else{
         alert("Username or Password is incorrect.")
